319f55d76c045e6b6000073260db5c5c31a281e7
EonCyrus <92433506+EonCyrus@users.noreply.github.com>
2021-10-13 15:09:44 +0530

    Add files via upload

---
+++ qb-multicharacter-master/__resource.lua
@@ -0,0 +1,21 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page "html/index.html"
+
+client_scripts {
+    'client/main.lua',
+    'client/cameras.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/reset.css',
+    'html/script.js',
+    'html/CYRUSus-logo.png',
+    'html/images/*.png',
+}
